601136624be1ff3482bf8d49349efecb3738ffc0
hamster1963 <1410514192@qq.com>
2024-12-12 22:13:59 +0800

    feat: nav indicator

--- src/components/xui/navigation-menu.tsx
+++ src/components/xui/navigation-menu.tsx
@@ -1,8 +1,17 @@
 import { NavigationMenuLinkProps, NavigationMenuTriggerProps } from "@radix-ui/react-navigation-menu"
 import { NavigationMenuLink, NavigationMenuTrigger, navigationMenuTriggerStyle } from "../ui/navigation-menu"
+import { motion } from "framer-motion";
 
 export const NzNavigationMenuLink = (props: NavigationMenuLinkProps & React.RefAttributes<HTMLAnchorElement>) => {
-    return <NavigationMenuLink {...props} className={navigationMenuTriggerStyle() + " hover:bg-inherit data-[active]:bg-inherit transition-colors text-foreground/60 data-[active]:text-foreground hover:text-foreground/90"} />
+    return <div className="relative">
+        <NavigationMenuLink {...props} className={navigationMenuTriggerStyle() + " hover:bg-inherit data-[active]:bg-inherit transition-colors text-foreground/60 data-[active]:text-foreground hover:text-foreground/90"} />
+        {props.active && (
+            <motion.div
+                layoutId="tab-underline"
+                className="absolute bottom-0 left-0 right-0 h-[2px] bg-black dark:bg-white"
+            />
+        )}
+    </div>
 }
 
 export const NzNavigationMenuTrigger = (props: Omit<NavigationMenuTriggerProps & React.RefAttributes<HTMLButtonElement>, "ref"> & React.RefAttributes<HTMLButtonElement>) => {
